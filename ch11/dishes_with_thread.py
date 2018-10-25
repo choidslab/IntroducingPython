b21c7b656e01b3306d23a16db7113760087be37a
dslab0915 <dslab0915@gmail.com>
2018-10-25 22:40:15 +0900

    Added thread example codes.

---
+++ ch11/dishes_with_thread.py
@@ -0,0 +1,24 @@
+"""11.1.3 Thread"""
+import threading, queue
+import time
+
+def washer(dishes, dish_queue):
+    for dish in dishes:
+        print('Washing', dish)
+        time.sleep(5)
+        dish_queue.put(dish)
+
+def dryer(dish_queue):
+    while True:
+        dish = dish_queue.get()
+        print('Drying', dish)
+        dish_queue.task_done()
+
+dish_queue = queue.Queue()
+for n in range(2):
+    dryer_thread = threading.Thread(target=dryer, args=(dish_queue, ))
+    dryer_thread.start()
+
+dishes = ['salad', 'bread', 'entree', 'desert']
+washer(dishes, dish_queue)
+dish_queue.join()
